6eb3f9114e75c47cf852803fdb415dd8d71e04d4
Dewansh Rawat <dewanshrawat15@gmail.com>
2019-07-28 21:51:24 +0530

    Updated Search

--- lib/search.dart
+++ lib/search.dart
@@ -29,12 +29,14 @@ class SearchScreenState extends State<SearchScreen> with SingleTickerProviderSta
     super.dispose();
   }
 
+  FocusNode myFocusNode = new FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(32.0),
+          padding: EdgeInsets.all(18.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -42,26 +44,24 @@ class SearchScreenState extends State<SearchScreen> with SingleTickerProviderSta
                 padding: EdgeInsets.all(32.0),
               ),
               TextField(
+                focusNode: myFocusNode,
                 textCapitalization: TextCapitalization.words,
                 textInputAction: TextInputAction.send,
                 cursorColor: theme_color,
                 style: TextStyle(color: theme_color),
                 decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: theme_color),
+                  enabledBorder: new UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: theme_color),
+                  focusedBorder: new UnderlineInputBorder(
+                    borderSide: BorderSide(color: theme_color, width: 1.0),
                   ),
-                  border: OutlineInputBorder(),
                   icon: Icon(Icons.search, color: theme_color),
                   labelText: 'Enter song name',
+                  labelStyle: TextStyle(color: theme_color),
                 ),
               ),
               songlist,
-              Padding(
-                padding: EdgeInsets.all(32.0),
-              ),
             ],
           ),
         ),
